ae82c2bc61c32f9984b2ae45db5b1dc305b1bbfc
Andrés3006 <edwin.vasquez3006@gmail.com>
2026-04-11 07:04:10 -0500

    conexion backend con base de datos funcionando

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const app = express();
 
+const testRoutes = require('./routes/test.routes');
+
 app.use(express.json());
 
+// rutas
+app.use('/api', testRoutes);
+
 app.get('/', (req, res) => {
-  res.send('API funcionando 🚀');
+    res.send('API funcionando 🚀');
 });
 
 app.listen(3000, () => {
-  console.log('Servidor en puerto 3000');
+    console.log('Servidor en puerto 3000');
 });
 
-app.get('/test', (req, res) => {
-  res.send('Funciona endpoint test');
+const pool = require('./db/connection');
+
+app.get('/db-test', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT NOW()');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
